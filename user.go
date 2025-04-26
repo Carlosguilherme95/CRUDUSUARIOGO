efa61d959ca452f4168683b84a91d9e978bbabf1
carlos <carlos_buske@hotmail.com>
2025-04-26 18:46:43 -0300

    crud

--- user.go
+++
@@ -1,8 +0,0 @@
-package entities
-
-type User struct {
-	ID        uint   `gorm:"primaryKey"`
-	FirstName string `json:"FirstName"`
-	LastName  string `json:"LastName"`
-	Email     string `json:"Email"`
-}
